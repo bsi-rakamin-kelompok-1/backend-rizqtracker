324e330a2e94c20c2ca45a3061ba08ef1f8eef24
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-18 19:32:26 +0700

    init project

---
+++ src/main/java/id/co/bankbsi/rizqtracker/RizqtrackerApplication.java
@@ -0,0 +1,13 @@
+package id.co.bankbsi.rizqtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizqtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizqtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/bankbsi/rizqtracker/RizqtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.bankbsi.rizqtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RizqtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
