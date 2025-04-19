c5dc77fccf9edc5805451df15714483af4758831
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-19 10:45:39 +0700

    feat: add AuthController for user registration and login endpoints

--- src/main/java/id/co/bankbsi/rizqtracker/controller/AuthController.java
+++ src/main/java/id/co/bankbsi/rizqtracker/controller/AuthController.java
@@ -1,4 +1,44 @@
 package id.co.bankbsi.rizqtracker.controller;
 
+import id.co.bankbsi.rizqtracker.dto.request.LoginRequest;
+import id.co.bankbsi.rizqtracker.dto.request.RegisterRequest;
+import id.co.bankbsi.rizqtracker.dto.response.LoginResponse;
+import id.co.bankbsi.rizqtracker.dto.response.RegisterResponse;
+import id.co.bankbsi.rizqtracker.model.User;
+import id.co.bankbsi.rizqtracker.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/auth")
 public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@Valid @RequestBody RegisterRequest req) {
+        User createdUser = this.userService.register(req);
+        RegisterResponse registerResponse = RegisterResponse.fromUser(createdUser);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(registerResponse);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest req) {
+        LoginResponse loginResponse = new LoginResponse();
+
+        String token = this.userService.login(req);
+
+        loginResponse.setStatus(true);
+        loginResponse.setMessage("Login success");
+        loginResponse.setToken(token);
+
+        return ResponseEntity.status(HttpStatus.OK).body(loginResponse);
+    }
 }
