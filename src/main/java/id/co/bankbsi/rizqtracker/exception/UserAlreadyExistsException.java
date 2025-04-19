06c8cfb0f1a338e1f22d9e268a18129a2f9778a0
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-19 10:45:16 +0700

    feat: add custom exception classes for password mismatch and user existence

--- src/main/java/id/co/bankbsi/rizqtracker/exception/UserAlreadyExistsException.java
+++ src/main/java/id/co/bankbsi/rizqtracker/exception/UserAlreadyExistsException.java
@@ -1,4 +1,7 @@
 package id.co.bankbsi.rizqtracker.exception;
 
-public class UserAlreadyExistsException {
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException (String message) {
+        super(message);
+    }
 }
