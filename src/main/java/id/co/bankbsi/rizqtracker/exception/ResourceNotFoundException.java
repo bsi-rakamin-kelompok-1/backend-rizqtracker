b1a2ddd234e0760c664200fb14545f831f100fb1
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-19 23:39:49 +0700

    feat: add ResourceNotFoundException for handling resource not found scenarios

---
+++ src/main/java/id/co/bankbsi/rizqtracker/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package id.co.bankbsi.rizqtracker.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
