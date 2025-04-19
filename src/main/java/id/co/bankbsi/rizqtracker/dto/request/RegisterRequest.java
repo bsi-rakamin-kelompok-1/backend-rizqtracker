7fcca91d0d3ed44b4a89b67fcd39672e409acc0a
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-19 10:42:52 +0700

    feat: enhance RegisterRequest validation with email, password, and user details

--- src/main/java/id/co/bankbsi/rizqtracker/dto/request/RegisterRequest.java
+++ src/main/java/id/co/bankbsi/rizqtracker/dto/request/RegisterRequest.java
@@ -1,12 +1,27 @@
 package id.co.bankbsi.rizqtracker.dto.request;
 
+import id.co.bankbsi.rizqtracker.validation.ValidPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is not valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @ValidPassword
     private String password;
+
+    @NotBlank(message = "Confirm password is required")
+    @ValidPassword
     private String confirmPassword;
+
+    @NotBlank(message = "Full name is required")
     private String fullName;
+
+    @NotBlank(message = "Phone number is required")
     private String phoneNumber;
 }
