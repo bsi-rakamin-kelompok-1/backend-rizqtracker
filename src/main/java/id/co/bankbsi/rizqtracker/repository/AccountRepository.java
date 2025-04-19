2ee706259575ba95370fe586c0ec902d1c912157
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-20 01:52:22 +0700

    feat: add method to find account by user ID in AccountRepository

--- src/main/java/id/co/bankbsi/rizqtracker/repository/AccountRepository.java
+++ src/main/java/id/co/bankbsi/rizqtracker/repository/AccountRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     public Optional<Account> findByAccountNumber(Long accountNumber);
+    public Optional<Account> findByUserId(Integer id);
 }
