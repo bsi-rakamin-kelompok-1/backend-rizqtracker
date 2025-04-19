6f93fbe66a0ac7f279a365108ce0d2c1500a7da7
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-19 23:39:45 +0700

    feat: add InsufficientBalanceException for handling insufficient balance scenarios

---
+++ src/main/java/id/co/bankbsi/rizqtracker/exception/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package id.co.bankbsi.rizqtracker.exception;
+
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
