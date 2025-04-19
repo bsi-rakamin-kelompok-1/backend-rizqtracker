96a9744b304d2474d8a44b5f87e8c3022a554509
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-19 10:43:24 +0700

    feat: add methods to UserRepository for email and phone number existence checks

--- src/main/java/id/co/bankbsi/rizqtracker/repository/UserRepository.java
+++ src/main/java/id/co/bankbsi/rizqtracker/repository/UserRepository.java
@@ -4,6 +4,11 @@ import id.co.bankbsi.rizqtracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
+    Optional<User> findUserByEmail(String email);
 }
