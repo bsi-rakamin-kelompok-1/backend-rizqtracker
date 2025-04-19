138798166bf7ec6a881d99b4484304b6f595b4e1
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-19 10:45:45 +0700

    feat: add TransactionController with endpoint to retrieve all transactions

--- src/main/java/id/co/bankbsi/rizqtracker/controller/TransactionController.java
+++ src/main/java/id/co/bankbsi/rizqtracker/controller/TransactionController.java
@@ -1,4 +1,16 @@
 package id.co.bankbsi.rizqtracker.controller;
 
+import id.co.bankbsi.rizqtracker.dto.response.BaseResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/transactions")
 public class TransactionController {
+    @GetMapping
+    public ResponseEntity<BaseResponse> getAllTransactions() {
+        return ResponseEntity.ok(new BaseResponse(true, "List of all transactions"));
+    }
 }
