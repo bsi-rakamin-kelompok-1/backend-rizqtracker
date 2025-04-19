6ff01a70e7fa09038aed8d52391de0f8d96b4d14
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-19 23:40:26 +0700

    feat: add SecurityUtil for extracting current user ID from JWT token

---
+++ src/main/java/id/co/bankbsi/rizqtracker/util/SecurityUtil.java
@@ -0,0 +1,25 @@
+package id.co.bankbsi.rizqtracker.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+public class SecurityUtil {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public Integer getCurrentUserId() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            return this.jwtUtil.extractUserId(token);
+        }
+
+        return null;
+    }
+}
