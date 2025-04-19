277e2f16197bfbceda5cdce159adbc33c6a5b070
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-20 01:54:46 +0700

    feat: implement transfer and topup endpoints with corresponding request and response DTOs

--- src/main/java/id/co/bankbsi/rizqtracker/controller/TransactionController.java
+++ src/main/java/id/co/bankbsi/rizqtracker/controller/TransactionController.java
@@ -1,18 +1,18 @@
 package id.co.bankbsi.rizqtracker.controller;
 
-import id.co.bankbsi.rizqtracker.dto.request.TransactionRequest;
+import id.co.bankbsi.rizqtracker.dto.request.TopupRequest;
+import id.co.bankbsi.rizqtracker.dto.request.TransferRequest;
 import id.co.bankbsi.rizqtracker.dto.response.BaseResponse;
+import id.co.bankbsi.rizqtracker.dto.response.TopupResponse;
 import id.co.bankbsi.rizqtracker.dto.response.TransactionResponse;
+import id.co.bankbsi.rizqtracker.dto.response.TransferResponse;
 import id.co.bankbsi.rizqtracker.model.Transaction;
 import id.co.bankbsi.rizqtracker.service.TransactionService;
-import id.co.bankbsi.rizqtracker.util.JwtUtil;
 import id.co.bankbsi.rizqtracker.util.SecurityUtil;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -30,17 +30,24 @@ public class TransactionController {
         return ResponseEntity.ok(new BaseResponse(true, "userId: " + securityUtil.getCurrentUserId()));
     }
 
-    @PostMapping("/v1/transactions")
-    public ResponseEntity<TransactionResponse> createTransaction(
-            @RequestParam(name = "type") String type,
-            @Valid @RequestBody TransactionRequest req) {
-        Transaction newTransaction = this.transactionService.createTransaction(type, req);
+    @PostMapping("/transfer")
+    public ResponseEntity<TransferResponse> createTransfer(
+            @Valid @RequestBody TransferRequest req) {
+        Transaction newTransfer = this.transactionService
+                .createTransfer(req, this.securityUtil.getCurrentUserId());
 
-        TransactionResponse response = new TransactionResponse();
-        response.setSuccess(true);
-        response.setMessage("Transaction created successfully");
-        response.setData(newTransaction);
-
+        TransferResponse response = TransferResponse.from(newTransfer);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @PostMapping("/topup")
+    public ResponseEntity<TopupResponse> createTopup(
+            @Valid @RequestBody TopupRequest req) {
+        Transaction newTopup = this.transactionService
+                .createTopup(req, this.securityUtil.getCurrentUserId());
+
+        TopupResponse response = TopupResponse.from(newTopup);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
 }
