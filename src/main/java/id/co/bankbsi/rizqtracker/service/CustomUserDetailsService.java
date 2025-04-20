c32a44c8eb37623d67bcf932a58d6e5a8a702493
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-20 21:31:00 +0700

    fix: remove commented-out user ID retrieval in CustomUserDetailsService

--- src/main/java/id/co/bankbsi/rizqtracker/service/CustomUserDetailsService.java
+++ src/main/java/id/co/bankbsi/rizqtracker/service/CustomUserDetailsService.java
@@ -5,9 +5,9 @@ import id.co.bankbsi.rizqtracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.Collections;
 import java.util.Optional;
@@ -27,7 +27,6 @@ public class CustomUserDetailsService implements UserDetailsService {
         return new org.springframework.security.core.userdetails.User(
                 user.get().getEmail(),
                 user.get().getPassword(),
-//                user.get().getId(),
                 Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
         );
     }
