28f4cfe74503d5a940872fd46951261f7ca63812
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-20 23:05:22 +0700

    feat: rename IncomeCashflowResponse to CashflowIncomeResponse

---
+++ src/main/java/id/co/bankbsi/rizqtracker/dto/response/CashflowExpenseResponse.java
@@ -0,0 +1,32 @@
+package id.co.bankbsi.rizqtracker.dto.response;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class CashflowExpenseResponse extends BaseCashflowResponse {
+    private ExpenseDetails expenseDetails;
+
+    @Data
+    public static class ExpenseDetails {
+        private List<TransferData> needs;
+        private List<TransferData> bills;
+        private List<TransferData> shopping;
+        private List<TransferData> transport;
+        private List<TransferData> transferOfWealth;
+    }
+
+    @Data
+    public static class TransferData {
+        private String transactionId;
+        private String recipientFullName;
+        private Long recipientAccountNumber;
+        private Long amount;
+        private String notes;
+        private LocalDateTime createdAt;
+    }
+}
