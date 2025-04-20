30436977c440cf9bb9685cace87ad5a15265f733
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-20 22:51:34 +0700

    feat: remove unnecessary NotNull validation from TopupRequest

--- src/main/java/id/co/bankbsi/rizqtracker/dto/request/TopupRequest.java
+++ src/main/java/id/co/bankbsi/rizqtracker/dto/request/TopupRequest.java
@@ -2,7 +2,6 @@ package id.co.bankbsi.rizqtracker.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
